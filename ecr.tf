1e38c8cc242b4e9740896af0b2b1a17e0c601f3c
anu.opp <anu.op@outlook.com>
2025-05-27 00:17:54 +0100

    files

---
+++ ecr.tf
@@ -0,0 +1,37 @@
+# ECR Repository
+resource "aws_ecr_repository" "ecommerce" {
+  name                 = "${var.project_name}-backend"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+
+  tags = {
+    Name        = "${var.project_name}-backend"
+    Environment = var.environment
+  }
+}
+
+# ECR Repository Policy
+resource "aws_ecr_lifecycle_policy" "ecommerce" {
+  repository = aws_ecr_repository.ecommerce.name
+
+  policy = jsonencode({
+    rules = [
+      {
+        rulePriority = 1
+        description  = "Keep last 10 images"
+        selection = {
+          tagStatus     = "tagged"
+          tagPrefixList = ["v"]
+          countType     = "imageCountMoreThan"
+          countNumber   = 10
+        }
+        action = {
+          type = "expire"
+        }
+      }
+    ]
+  })
+}
---
+++ src/main/java/com/cyat/ecommerce/EcommerceApplication.java
@@ -0,0 +1,12 @@
+package com.cyat.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EcommerceApplication.class, args);
+        System.out.println("E-commerce Backend Running...");
+    }
+}
