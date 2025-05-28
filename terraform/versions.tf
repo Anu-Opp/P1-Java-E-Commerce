27ad8218067912ac9d924f805c62921129f63a94
anu.opp <anu.op@outlook.com>
2025-05-29 00:48:06 +0100

    file update

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -13,10 +13,6 @@ terraform {
   }
 }
 
-provider "aws" {
-  region = var.aws_region
-}
-
 provider "kubernetes" {
   host                   = module.eks.cluster_endpoint
   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
