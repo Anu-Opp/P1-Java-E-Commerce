640eb5e05beb6c6c42d66c06014855f8922458dc
anu.opp <anu.op@outlook.com>
2025-05-29 01:02:35 +0100

    file update

--- setup_backend.sh
+++ setup_backend.sh
@@ -6,7 +6,11 @@ S3_BUCKET_NAME="ceeyit-terraform-state-bucket"
 DYNAMODB_TABLE_NAME="terraform-locks"
 
 # Create S3 Bucket (only if it doesn't exist)
-aws s3api create-bucket --bucket $S3_BUCKET_NAME --region $AWS_REGION --create-bucket-configuration LocationConstraint=$AWS_REGION || echo "Bucket may already exist"
+if [ "$AWS_REGION" = "us-east-1" ]; then
+  aws s3api create-bucket --bucket $S3_BUCKET_NAME || echo "Bucket may already exist"
+else
+  aws s3api create-bucket --bucket $S3_BUCKET_NAME --region $AWS_REGION --create-bucket-configuration LocationConstraint=$AWS_REGION || echo "Bucket may already exist"
+fi
 
 # Enable versioning on the bucket
 aws s3api put-bucket-versioning --bucket $S3_BUCKET_NAME --versioning-configuration Status=Enabled
